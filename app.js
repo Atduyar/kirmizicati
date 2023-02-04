01c17d09f7f35a26f6a82d3bc2ecd27f77524411
Ahmet Tarık Duyar(Atduyar) <atduyar@gmail.com>
2023-02-05 02:47:35 +0300

    add dotenv, .env.template

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 import express from "express"
+import dotenv  from "dotenv";
+
+dotenv.config({ path: './.env' });
+
 const app = express()
-const port = 5000
+const port = process.env.PORT || 5000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')

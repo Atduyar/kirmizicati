b1b95dad5dbc08f2adc15ead4ea52beff187525e
Ahmet Tarık Duyar(Atduyar) <atduyar@gmail.com>
2023-02-05 10:16:08 +0300

    basic get create

--- app.js
+++ app.js
@@ -1,16 +1,40 @@
 import express from "express"
 import dotenv  from "dotenv";
 
+//Config
 dotenv.config({ path: './.env' });
-
-const app = express()
 const port = process.env.PORT || 5000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+//Init
+const prisma = new PrismaClient()
+const app = express()
+
+app.get('/data', async (req, res) => {
+	res.send('Hello World!')
+})
+app.get('/data', async (req, res) => {
+	const post = await prisma.data.findMany()
+	res.json(post)
+})
+app.get('/data:id', async (req, res) => {
+	const { id } = req.params
+	const post = await prisma.data.findUnique({
+		where: { id }
+	})
+	res.json(post)
+})
+
+app.post('/post', async (req, res) => {
+	const { title, content, authorEmail } = req.body
+	const post = await prisma.data.create({
+		data: {
+			...req.body
+		},
+	})
+	res.json(post)
 })
 
 app.listen(port, () => {
-  console.log(`http://localhost:${port}`)
-  console.log(`Example app listening on port ${port}`)
+	console.log(`http://localhost:${port}`)
+	console.log(`Example app listening on port ${port}`)
 })
--- readme.md
+++ readme.md
@@ -1,10 +1,12 @@
 # Todo
-- [ ] Cihaz Id
-- [ ] Kullanıcı Id
-- [ ] Tarih Saat
-- [ ] Değer1
-- [ ] Değer2
-- [ ] Kural1
-- [ ] Kural2
-- [ ] Sonuç
-- [ ] Bildirim
+- [ ] Github Action
+- [ ] Tablolar
+    - Cihaz Id
+    - Kullanıcı Id
+    - Tarih Saat
+    - Değer1
+    - Değer2
+    - Kural1
+    - Kural2
+    - Sonuç
+    - Bildirim
